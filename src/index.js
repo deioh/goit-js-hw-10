24ff4c0902cbebab95d2719df5321d1d4e635687
deioh <143625578+deioh@users.noreply.github.com>
2024-05-26 23:29:45 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-const base_url = 'https://api.thecatapi.com';
-//  / v1 / images / search ? limit = 10 & breed_ids=beng& api_key=live_zHFebZByr5MT95LL79HO8n98KLXjkHMmP1yvgYlPDH5xs1LuljJJtWZCJqZ4WUZV';
+import axios from 'axios';
 
-export function fetchCats() {
-  return fetch(`${base_url}/v1/breeds?limit=10&page=0`).then(res => {
-    if (!res.ok) throw new error(res.status);
-    return res.json();
+axios.defaults.headers.common['x-api-key'] =
+  'live_zHFebZByr5MT95LL79HO8n98KLXjkHMmP1yvgYlPDH5xs1LuljJJtWZCJqZ4WUZV';
+
+axios
+  .get('https://api.thecatapi.com/v1/breeds')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
   });
-}
+
+const selectEL = document.getElementsByClassName('breed-select');
+
+selectEL.addEventListener('change', () => {
+  //const value = event.target.value;
+  //console.log('selectEL value');
+  alert('select EL value');
+});
