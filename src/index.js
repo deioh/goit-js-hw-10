fe2e72f7c66e9b2322b636ae203e05811638b913
deioh <143625578+deioh@users.noreply.github.com>
2024-05-31 21:30:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
+const catInfoDiv = document.querySelector('.cat-info');
+let cat;
 
 breedSelect.addEventListener('change', event => {
   console.log('Change event occurred');
   const breedId = event.target.value;
   console.log(`Selected breed ID: ${breedId}`);
-  fetchCatByBreed(breedId);
+
+  cat = fetchCatByBreed(breedId);
+  console.log('cat breed fetch', cat);
+  cat_Info(breedId);
 });
 
 console.log('Starting fetchBreeds...');
@@ -28,3 +33,28 @@ fetchBreeds()
   .catch(error => {
     console.error('Error fetchingBreeds:', error);
   });
+
+function cat_Info(breedId) {
+  console.log('Starting catinfo...');
+
+  const test = Object.entries(cat).map([test.url, cat.url]);
+
+  console.log('test', test);
+}
+// .then(breeds => {
+//   console.log(`fetchBreeds successful. Received ${breeds.length} breeds`);
+//   const options = breeds
+//     .map(breed => {
+//       const option = document.createElement('option');
+//       option.value = breed.id;
+//       option.textContent = breed.name;
+//       return option;
+//     })
+//     .forEach(option => {
+//       console.log(`Appending option ${option.textContent} (${option.value})`);
+//       breedSelect.appendChild(option);
+//     });
+// })
+// .catch(error => {
+//   console.error('Error fetchingBreeds:', error);
+// });
